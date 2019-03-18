b7388a5156910f2f073e00411be91667e15e4463
David Espinosa <david.emanuel.espinosa@gmail.com>
2019-03-18 02:43:07 -0300

    Ejercicio final resuelto

--- src/app/ejercicio-servicio-dolar/dolar.service.ts
+++ src/app/ejercicio-servicio-dolar/dolar.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { throwError, Observable } from 'rxjs';
 import { map, catchError, delay } from 'rxjs/operators';
 import { Dolar } from './dolar';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,7 @@ export class DolarService {
   constructor(private http: HttpClient) { }
 
   public getDolar(): Observable<Dolar> {
-      return this.http.get('mocks/mock-dolar.json')
+      return this.http.get(environment.getDolarAPI)
         .pipe(
           delay(3000),
           map((response: Dolar) => response),
